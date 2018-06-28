1d8583cfec7bd583316ff2f1efab77d2a1844012
lp1 <mail@lp1.eu>
2018-06-28 22:16:50 +0200

    Added exports to mirage.ts

--- dist/mirage.d.ts
+++ dist/mirage.d.ts
@@ -1,2 +1,5 @@
 import { Game } from "./game/class";
-export { Game };
+import { Question } from './questions-builder/interfaces';
+import { GameData, SaveData } from './game/interfaces';
+import { State } from './state-machine/interfaces';
+export { Game, Question, GameData, SaveData, State };
--- src/mirage.ts
+++ src/mirage.ts
@@ -1,3 +1,6 @@
 import { Game } from "./game/class";
+import { Question } from './questions-builder/interfaces';
+import { GameData, SaveData} from './game/interfaces';
+import { State } from './state-machine/interfaces';
 
-export { Game }
+export { Game, Question, GameData, SaveData, State }
