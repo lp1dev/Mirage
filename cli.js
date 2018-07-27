2d29fed7477c2a72748dca8f4fa5c42b47065eb6
lp1 <mail@lp1.eu>
2018-07-28 01:38:55 +0200

    Added CLI util to compile games from markdown files

---
+++ cli.js
@@ -0,0 +1,80 @@
+#!/bin/env node
+(function() {
+  const fs = require('fs');
+  const argv = process.argv;
+
+  function usage() {
+    console.log(`${argv[1]}: game.md locale\nexample: ${argv[1]}: mygame.md EN_en`)
+    return -1;
+  }
+
+  if (argv.length !== 4) {
+    process.exit(usage())
+  }
+  fs.readFile(argv[2], {encoding: 'utf-8'}, (error, data) => {
+    if (error && error.errno !== 0) {
+      console.error(`${error}`) && process.exit(error.errno);
+    } else {
+      const gameData = parseData(data);
+      writeOutput(argv[2], gameData, argv[3]);
+    }
+  });
+
+  function parseData(markdownData) {
+    const lines = markdownData.split(/(\n|\r)/g);
+    const game = { questions: [] };
+    let questionBuffer = null;
+
+    lines.forEach((line) => {
+      let lineBeginning = line.split(/\s+/g)[0];
+      if (!['##', '#', '`', ' ', '-'].includes(lineBeginning)) {
+        lineBeginning = line[0];
+      }
+      const trimmedLine = line.replace(lineBeginning, '').trim();
+      switch(lineBeginning) {
+        case '#':
+          game['name'] = trimmedLine;
+          break;
+        case '##':
+          if (questionBuffer) {
+            game['questions'].push(questionBuffer);
+          }
+          questionBuffer = { id: trimmedLine }
+          break;
+        case ' ':
+          if (questionBuffer['text']) {
+            questionBuffer['text'] += trimmedLine + '\n';
+          } else {
+            questionBuffer['text'] = trimmedLine + '\n';
+          }
+          break;
+        case '-':
+          if (!questionBuffer['answers']) {
+            questionBuffer['answers'] = {};
+          }
+          const splitQuestion = trimmedLine.split('`');
+          questionBuffer['answers'][splitQuestion[0].trim()] = 
+            splitQuestion[1].replace('`', '');
+          break;
+        case '`':
+          game['startInstruction'] = trimmedLine.replace('`', '');
+          break;
+      }
+    });
+    return game;    
+  }
+
+  function writeOutput(URI, gameData, locale) {
+    const outputFileName = URI.replace('.md', `_${locale}.json`);
+    fs.writeFile(outputFileName, 
+                JSON.stringify(gameData), 
+                {encoding: 'utf-8'},
+                (error) => {
+                  if (error && error.errno !== 0) {
+                    console.error(`${error}`) && process.exit(error.errno);
+                  } else {
+                    console.log(`${outputFileName} written.`)
+                  }
+                });
+  }
+})();
---
+++ src/mock/game.md
@@ -0,0 +1,27 @@
+# Awesome Test Game - Volume I
+
+`goto gameIntro`
+
+## gameIntro
+
+  Vous vous reveillez, les mains attachées ensemble. 
+  Que choisissez-vous de faire ?
+
+- Ne rien faire. `goto gameIntro1`
+- Tirer sur ses liens `goto gameIntro2`
+- Crier `goto gameIntro1`
+
+## gameIntro1
+
+  Vous entendez des ronflements dans la cellule d'en face,
+  aucune réaction de la part du dormeur.
+
+- Ne rien faire. `goto gameIntro1`
+- Tirer sur ses liens `goto gameIntro2`
+- Crier `goto gameIntro1`
+
+## gameIntro2
+
+  Vous vous êtes enfin libéré ! Bravo !
+
+- Retourner au début `goto GameIntro1`
